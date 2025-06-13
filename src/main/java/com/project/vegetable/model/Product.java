4653d22233e79a4e27573883d8019e35c87fde28
Alexandra Eriksson <maje@hotmail.se>
2025-06-13 13:19:10 +0200

    Changed dependencies and started to create first entity.

---
+++ src/main/java/com/project/vegetable/model/Product.java
@@ -0,0 +1,11 @@
+package com.project.vegetable.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+class Product {
+}
